b5a85a23c553237559948cba03195adc27ef754f
Broc Gailit <brocgailit@gmail.com>
2019-03-31 16:13:04 -0700

    Check plugins loaded

--- co-authors-plus-rest-api.php
+++ co-authors-plus-rest-api.php
@@ -5,31 +5,34 @@
  * Version: 0.0.2
  * License: GPL2+
 */
-if ( function_exists('get_coauthors') ) {
-    add_action( 'rest_api_init', 'custom_register_coauthors' );
-    function custom_register_coauthors() {
-        register_rest_field( 'post',
-            'coauthors',
-            array(
-                'get_callback'    => 'custom_get_coauthors',
-                'update_callback' => null,
-                'schema'          => null,
-            )
-        );
-    }
-
-    function custom_get_coauthors( $object, $field_name, $request ) {
-        $coauthors = get_coauthors($object['id']);
-
-        $authors = array();
-        foreach ($coauthors as $author) {
-            $authors[] = array(
-                'id' => $author->id,
-                'display_name' => $author->display_name,
-                'user_nicename' => $author->user_nicename
+function init_endpoints() {
+    if ( function_exists('get_coauthors') ) {
+        add_action( 'rest_api_init', 'custom_register_coauthors' );
+        function custom_register_coauthors() {
+            register_rest_field( 'post',
+                'coauthors',
+                array(
+                    'get_callback'    => 'custom_get_coauthors',
+                    'update_callback' => null,
+                    'schema'          => null,
+                )
             );
-        };
+        }
 
-        return $authors;
+        function custom_get_coauthors( $object, $field_name, $request ) {
+            $coauthors = get_coauthors($object['id']);
+
+            $authors = array();
+            foreach ($coauthors as $author) {
+                $authors[] = array(
+                    'id' => $author->id,
+                    'display_name' => $author->display_name,
+                    'user_nicename' => $author->user_nicename
+                );
+            };
+
+            return $authors;
+        }
     }
 }
+add_action( 'plugins_loaded', 'init_endpoints');
